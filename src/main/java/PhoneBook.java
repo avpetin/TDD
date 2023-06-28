7193d07bfd5dd6ed661990b7dcbac856dd3b57c1
Alexey Petin <alex_beholder@mail.ru>
2023-06-28 17:05:26 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, Integer> phbMap = new HashMap<>();
     public int add(String name, Integer number){
+        phbMap.put(name, number);
         return 0;
+    }
+
+    public String findByNumbers(){
+        return null;
+    }
+
+    public Integer getPhonebookSize(){
+        return phbMap.size();
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -3,20 +3,25 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.mockito.Mockito.verify;
 
 public class PhoneBookTest {
+    PhoneBook sut = new PhoneBook();
     @Test
     public void testAds(){
         //arrange
-        PhoneBook phoneBook = Mockito.mock(PhoneBook.class);
+/*        PhoneBook phoneBook = Mockito.mock(PhoneBook.class);
         Mockito.when(phoneBook.add(Mockito.anyString(), Mockito.anyInt()))
                 .thenReturn(0);
         ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
-        verify(phoneBook).add(Mockito.anyString(), Mockito.anyInt());
-        Assertions.assertEquals(phoneBook.add(Mockito.anyString(), Mockito.anyInt()), argumentCaptor.getValue());
+        verify(phoneBook).add(Mockito.anyString(), Mockito.anyInt());*/
+
+        //arrange
+        int expected = 1, result;
+        //act
+        sut.add(Mockito.anyString(), Mockito.anyInt());
+        result = sut.getPhonebookSize();
+        //assert
+        Assertions.assertEquals(expected, result);
     }
 }
