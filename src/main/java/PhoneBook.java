b6c9e8d81dc7eec52f8fc33247f76c58c23007cc
Alexey Petin <alex_beholder@mail.ru>
2023-06-28 16:43:08 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,5 @@
 public class PhoneBook {
+    public int add(String name, Integer number){
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.verify;
+
 public class PhoneBookTest {
+    @Test
+    public void testAds(){
+        //arrange
+        PhoneBook phoneBook = Mockito.mock(PhoneBook.class);
+        Mockito.when(phoneBook.add(Mockito.anyString(), Mockito.anyInt()))
+                .thenReturn(0);
+        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+        verify(phoneBook).add(Mockito.anyString(), Mockito.anyInt());
+        Assertions.assertEquals(phoneBook.add(Mockito.anyString(), Mockito.anyInt()), argumentCaptor.getValue());
+    }
 }
